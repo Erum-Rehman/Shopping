8504c6bcddce55648f0d2886f0f0ed2f3fffce45
Erum-Rehman <erumr0443@gmail.com>
2023-02-22 00:27:26 +0500

    .change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,32 @@ import React from 'react';
 function Footer() {
     return (
         <>
-            <div className='container-fluid' style={{ width: "80%", backgroundColor: "aliceblue", marginTop:"2%" }} >
+        <br/>
+        
+                <div className="footer_div">
                 <div className=' left-side ' style={{ float: "left", }}>
                     <p>About Our store</p>
-                    <img src='https://www.olx.com.pk/assets/iconAppStoreEN_noinline.a731d99c8218d6faa0e83a6d038d08e8.svg' style={{ width: "25%" }}></img><br></br>
-                    <img src='https://www.olx.com.pk/assets/iconGooglePlayEN_noinline.9892833785b26dd5896b7c70b089f684.svg' style={{ width: "25%", marginTop: "0.5%" }}></img>
+                    <img src='https://www.olx.com.pk/assets/iconAppStoreEN_noinline.a731d99c8218d6faa0e83a6d038d08e8.svg' style={{ width: "35%" }}></img><br></br>
+                    <img src='https://www.olx.com.pk/assets/iconGooglePlayEN_noinline.9892833785b26dd5896b7c70b089f684.svg' style={{ width: "35%", marginTop: "2%" }}></img>
                 </div>
                 <div className='right-side' style={{ float: "right", paddingLeft: "20px" }}>
                     <div className="container">
                         <div className="row mt-4">
-                            <div className="col-lg-4 ">
+                            <div className="col-lg-4 footer_right">
                                 <h6 >ELECTRONICS</h6>
                                 <p>Mobiles</p>
                                 <p>Tablets</p>
                                 <p>Laptops</p>
                                 <p>Laptops</p>
                             </div>
-                            <div className="col-lg-4 ">
+                            <div className="col-lg-4 footer_right">
                                 <h6>ELECTRONICS</h6>
                                 <p>Mobiles</p>
                                 <p>Tablets</p>
                                 <p>Laptops</p>
                                 <p>Laptops</p>
                             </div>
-                            <div className="col-lg-4 ">
+                            <div className="col-lg-4 footer_right">
                                 <h6>ELECTRONICS</h6>
                                 <p>Buyone.info.com.hk</p>
                                 <p>(852)26900948</p>
@@ -37,9 +39,21 @@ function Footer() {
 
                     </div>
                 </div>
-
+                </div>
+                
+            <div className='prim_footer'>
+                <p>It is a long established fact that a reader will be distracted by the readable</p>
+                <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '2%', paddingRight: '4%'}}>
+                <p>2022 Buyone.com Online Store.jkdjsd</p>
+                <div style={{display: 'flex'}}>
+                <p className='px-3'>policy</p>
+                <p className='px-3'>policy</p>
+                <p className='px-3'>policy</p>
+                </div>
+               
+                </div>
             </div>
-            
         </>
     );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -8,12 +8,12 @@ function GridExample() {
         <Row xs={1} md={6} className="g-4">
       {Array.from({ length: 30 }).map((_, idx) => (
         <Col>
-          <Card style={{ width: '13rem', marginBottom: '4%', border: 'none' }}>
+          <Card style={{ width: '13rem', marginBottom: '6%', border: 'none' }}>
             <img src='../../../public/assets/custCase.png' style={{width: '100%', height: '185px'}}/>
            
               <p style={{fontSize: '14px'}}>This is a longer card with supporting text below as  natural</p>
                 <div style={{display: 'flex', justifyContent: 'space-between', margin: '2%'}}>
-                  <h5 style={{color: '#3B62AB'}}>$760.00</h5>
+                  <h5 style={{color: '#3B62AB', fontWeight: '700'}}>$760.00</h5>
                   <p style={{color: '#999999', fontSize: '12px', fontWeight: '600'}}>155oldPrice</p>
                 </div>
                 
